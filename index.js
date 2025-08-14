484eac78558e056338ba8477db7ba6961a85bb77
Amin <106281828+AminSaei@users.noreply.github.com>
2025-08-14 19:57:41 +0330

    Update the dice picture

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 function rollDice() {
-    const numOfDice = parseInt(document.getElementById("numOfDice").value);
-    const diceresult = document.getElementById("diceresult");
-    const diceimages = document.getElementById("diceimages");
-    const values = [];
-    const images = [];
+  const numOfDice = parseInt(document.getElementById("numOfDice").value);
+  const diceresult = document.getElementById("diceresult");
+  const diceimages = document.getElementById("diceimages");
+  const values = [];
+  const images = [];
 
-    diceimages.innerHTML = "";
+  diceimages.innerHTML = "";
 
-    for (let i = 0; i < numOfDice; i++) {
-        const value = Math.floor(Math.random() * 6) + 1;
-        values.push(value);
-        images.push(`<img src="dice/${value}.svg" alt="Dice ${value}" width="100">`);
-    }
+  for (let i = 0; i < numOfDice; i++) {
+    const value = Math.floor(Math.random() * 6) + 1;
+    values.push(value);
+    images.push(`<img src="${value}.svg" alt="Dice ${value}" width="100">`);
+  }
 
-    diceresult.textContent = `Dice: ${values.join(', ')}`;
-    diceimages.innerHTML = images.join('');
+  diceresult.textContent = `Dice: ${values.join(", ")}`;
+  diceimages.innerHTML = images.join("");
 }
+
