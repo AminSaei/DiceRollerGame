ef72aa2becface5662fdeefaa31d3a143e08f43a
Amin <106281828+AminSaei@users.noreply.github.com>
2025-08-14 19:44:46 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function rollDice() {
+    const numOfDice = parseInt(document.getElementById("numOfDice").value);
+    const diceresult = document.getElementById("diceresult");
+    const diceimages = document.getElementById("diceimages");
+    const values = [];
+    const images = [];
+
+    diceimages.innerHTML = "";
+
+    for (let i = 0; i < numOfDice; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="dice/${value}.svg" alt="Dice ${value}" width="100">`);
+    }
+
+    diceresult.textContent = `Dice: ${values.join(', ')}`;
+    diceimages.innerHTML = images.join('');
+}
